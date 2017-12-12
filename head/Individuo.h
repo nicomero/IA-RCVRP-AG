ef0353b5e783f0e9a4ba54db62bf5ba334705218
Nicomero <nicolas.alarcon.14@sansano.usm.cl>
2017-12-11 21:53:30 -0300

    comentarios agregados

--- head/Individuo.h
+++ head/Individuo.h
@@ -6,14 +6,14 @@
 
 class Individuo{
 public:
-    float calidad;
-    std::vector<Nodo> tour;
-    std::vector<int> retorno;
+    float calidad; //calida del individuo (mas calidad -> peor solucion)
+    std::vector<Nodo> tour; //orden en que recorre los nodos
+    std::vector<int> retorno;   //puntos en los que vuelve al almacen
 
-    Individuo(std::vector<Nodo> cities, float maxRiesgo);
-    float distancia(Nodo u, Nodo w);
-    float riesgo(float riesgoPrev, Nodo u, Nodo w, int demandAcum);
-    void mutar( float maxRiesgo );
-    void evaluar( float maxRiesgo );
+    Individuo(std::vector<Nodo> cities, float maxRiesgo);//constructor
+    float distancia(Nodo u, Nodo w);    //calcula la distancia entre 2 nodos
+    float riesgo(float riesgoPrev, Nodo u, Nodo w, int demandAcum);//calcula riesgo
+    void mutar( float maxRiesgo );  //muta la ruta
+    void evaluar( float maxRiesgo );    //evalua la ruta
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main(void)
 
     mundo.cruzaMasiva();
 
-    //mundo.mutarMasivo();
+    mundo.mutarMasivo();
 
 
 /*
